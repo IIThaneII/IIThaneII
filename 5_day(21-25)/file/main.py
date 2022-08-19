6ee7eb8d141ccaed27236a2b203debc2fbb5fc4a
Thành Nguyễn Ngọc <94160631+llThanell@users.noreply.github.com>
2022-08-19 23:11:47 +0700

    Delete 5_day(21-25) directory

--- 5_day(21-25)/file/main.py
+++
@@ -1,6 +0,0 @@
-# 🚨 write file 👇
-# with open("/Users/ez/OneDrive/Máy tính/codeprojects/PYTHON/100_days_of_Py/5_day(21-25)/file/my_file.txt", mode = "a") as file:
-# or
-with open("../../../OneDrive/Máy tính/codeprojects/PYTHON/100_days_of_Py/day(21-25)/file/my_file.txt", mode = "a") as file:
-    file.write("\nNew text.")
-    #"a" mode will add the content of the file without delete the data
