9913f4f39a8a4e020663e563144712157c2fe83e
Thành Nguyễn Ngọc <94160631+llThanell@users.noreply.github.com>
2022-08-19 23:11:38 +0700

    Delete 6_day(26-30) directory

--- 6_day(26-30)/data_overlap/main.py
+++
@@ -1,7 +0,0 @@
-with open("/Users/ez/OneDrive/Máy tính/codeprojects/PYTHON/100_days_of_Py/6_day(26-30)/data_overlap/file1.txt") as f1:
-    a = [int(num) for num in f1.readlines()]
-with open("/Users/ez/OneDrive/Máy tính/codeprojects/PYTHON/100_days_of_Py/6_day(26-30)/data_overlap/file2.txt") as f2:
-    b = [int(num) for num in f2.readlines()]
-
-c = [num for num in a if num in b]
-print(c)
